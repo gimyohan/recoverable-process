ea3ab926feda5062ebc6794ccdc6f8c458bcb4a1
javago <135432909+gimyohan@users.noreply.github.com>
2024-11-30 14:51:13 +0900

    Update first.c
    convert indent to tab

--- first.c
+++ first.c
@@ -24,105 +24,105 @@ int cur_read_count, cur_write_count;
 int data_pipe[2], meta_pipe[2];
 
 void setMetaData(){
-    int meta[3];
-    read(meta_pipe[0], meta, sizeof(int)*3);
-    meta[0] = cur_read_count, meta[1] = cur_write_count, meta[2] = window_size;
-    write(meta_pipe[1], &meta, sizeof(int)*3);
+	int meta[3];
+	read(meta_pipe[0], meta, sizeof(int)*3);
+	meta[0] = cur_read_count, meta[1] = cur_write_count, meta[2] = window_size;
+	write(meta_pipe[1], &meta, sizeof(int)*3);
 }
 
 void r_scanf(const char *format, void *p){
-    int sz = 0;
-    if(strcmp(format, "%d")==0) sz = sizeof(int); else
-    if(strcmp(format, "%ld")==0) sz = sizeof(long); else
-    if(strcmp(format, "%f")==0) sz = sizeof(float); else
-    if(strcmp(format, "%lf")==0) sz = sizeof(double); else
-    if(strcmp(format, "%c")==0) sz = sizeof(char); else
-    {printf("wrong format handled!\n"); exit(1);}
+	int sz = 0;
+	if(strcmp(format, "%d")==0) sz = sizeof(int); else
+	if(strcmp(format, "%ld")==0) sz = sizeof(long); else
+	if(strcmp(format, "%f")==0) sz = sizeof(float); else
+	if(strcmp(format, "%lf")==0) sz = sizeof(double); else
+	if(strcmp(format, "%c")==0) sz = sizeof(char); else
+	{printf("wrong format handled!\n"); exit(1);}
 
 	if(tgt_read_count<=cur_read_count){ //normal mode
 		scanf(format, p);
-        tgt_write_count = -1;
+		tgt_write_count = -1;
 	}
 	else{ //recovery mode
 		read(data_pipe[0], p, sz); queue_size-=sz;
 	}
-    write(data_pipe[1], p, sz); window_size+=sz;
-    cur_read_count++;
-    /*shrink to fit*/
-    if(cur_read_count==tgt_read_count||cur_read_count==1&&tgt_read_count==0){
+	write(data_pipe[1], p, sz); window_size+=sz;
+	cur_read_count++;
+	/*shrink to fit*/
+	if(cur_read_count==tgt_read_count||cur_read_count==1&&tgt_read_count==0){
 		void *dummy = malloc(queue_size);
 		read(data_pipe[0], dummy, queue_size);
 		free(dummy);
 	}
-    setMetaData();
+	setMetaData();
 }
 
 void r_printf(const char *format, ...){
-    if(tgt_write_count<=cur_write_count){
-        va_list args;
-        va_start(args, format);
-        vprintf(format, args);
-        va_end(args);
-    }
-    cur_write_count++;
-    setMetaData();
+	if(tgt_write_count<=cur_write_count){
+		va_list args;
+		va_start(args, format);
+		vprintf(format, args);
+		va_end(args);
+	}
+	cur_write_count++;
+	setMetaData();
 }
 
 void superInit(){
-    /*pipe for logging*/
-    pipe(data_pipe); pipe(meta_pipe);
-    /*ignore SIGINT*/
-    struct sigaction sa;
-    sa.sa_handler = SIG_IGN;
-    sigemptyset(&sa.sa_mask);
-    sigaction(SIGINT, &sa, NULL);
+	/*pipe for logging*/
+	pipe(data_pipe); pipe(meta_pipe);
+	/*ignore SIGINT*/
+	struct sigaction sa;
+	sa.sa_handler = SIG_IGN;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGINT, &sa, NULL);
 }
 void childInit(){
-    /*init meta_data*/
-    int meta[3];
-    cur_read_count = cur_write_count = 0;
-    meta[0] = cur_read_count, meta[1] = cur_write_count, meta[2] = window_size;
-    write(meta_pipe[1], &meta, sizeof(int)*3);
-    /*default SIGINT*/
-    struct sigaction sa;
-    sa.sa_handler = SIG_DFL;
-    sigemptyset(&sa.sa_mask);
-    sigaction(SIGINT, &sa, NULL);
+	/*init meta_data*/
+	int meta[3];
+	cur_read_count = cur_write_count = 0;
+	meta[0] = cur_read_count, meta[1] = cur_write_count, meta[2] = window_size;
+	write(meta_pipe[1], &meta, sizeof(int)*3);
+	/*default SIGINT*/
+	struct sigaction sa;
+	sa.sa_handler = SIG_DFL;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGINT, &sa, NULL);
 }
 
 void yourCode();
 int main(){
-    superInit();
-    int pid, status;
-    while(1){
-        pid = fork();
-        if(pid==0){
-            childInit();
-            yourCode();
-            exit(0);
-        }
-        waitpid(pid, &status, 0);
-        if(!WIFSIGNALED(status))break;
-        /*recovery process*/
-        int meta[3]; read(meta_pipe[0], &meta, sizeof(int)*3);
-        cur_read_count = meta[0], tgt_write_count = meta[1], queue_size = meta[2];
-        printf("입력번호는 %d; 원하는 입력번호는?\n", cur_read_count);
-        scanf("%d", &tgt_read_count);getchar();
-        /*boundary handling*/
-        if(tgt_read_count<0)tgt_read_count = 0;
-        if(cur_read_count<tgt_read_count)tgt_read_count = cur_read_count;
-        printf(" ... recovery completed ...\n");
-    }
-    return 0;
+	superInit();
+	int pid, status;
+	while(1){
+		pid = fork();
+		if(pid==0){
+			childInit();
+			yourCode();
+			exit(0);
+		}
+		waitpid(pid, &status, 0);
+		if(!WIFSIGNALED(status))break;
+		/*recovery process*/
+		int meta[3]; read(meta_pipe[0], &meta, sizeof(int)*3);
+		cur_read_count = meta[0], tgt_write_count = meta[1], queue_size = meta[2];
+		printf("입력번호는 %d; 원하는 입력번호는?\n", cur_read_count);
+		scanf("%d", &tgt_read_count);getchar();
+		/*boundary handling*/
+		if(tgt_read_count<0)tgt_read_count = 0;
+		if(cur_read_count<tgt_read_count)tgt_read_count = cur_read_count;
+		printf(" ... recovery completed ...\n");
+	}
+	return 0;
 }
 
 void sum10int(){
-    int i, n, sum = 0;
-    for(i=0;i<10;i++){
-        r_scanf("%d", &n);
-        sum+=n;
-        r_printf("sum=%d\n", sum);
-    }
+	int i, n, sum = 0;
+	for(i=0;i<10;i++){
+		r_scanf("%d", &n);
+		sum+=n;
+		r_printf("sum=%d\n", sum);
+	}
 }
 
 void sum10real(){
@@ -146,7 +146,7 @@ void sum10char(){
 }
 
 void yourCode(){
-    sum10int();
+	sum10int();
 	/*sum10real();*/
 	/*sum10char();*/
 }
